80f931953bdaeac189e11ce041430c12760f36c8
Herbertt <herbertt.carneiro@invisual.com.br>
2019-07-29 13:50:07 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Printer, PrintOptions } from '@ionic-native/printer/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public content = '<h1>MBA printer<h1><br><p>Aqui está um exemplo de utilização do plugin printer do ionic na versão 4';
 
-  constructor() {}
+  constructor(private printer: Printer) { }
+  
+  handlePrinter() {
+    this.printer.isAvailable().then(this.onSuccess, this.onError);
 
+    let options: PrintOptions = {
+      name: 'MyDocument',
+      printerId: 'printer007',
+      duplex: true,
+      landscape: true,
+      grayscale: true
+    }
+
+    this.printer.print(this.content, options).then(this.onSuccess, this.onError);
+  }
+
+  onSuccess() {
+    console.log('imprimindo...');
+    
+  }
+  
+  onError() {
+    console.log('error...');
+  }
 }
